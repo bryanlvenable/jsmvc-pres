a8270c07c7d3b93322466439e529afe63ef4127a
Bryan L. Venable <bryanlvenable@gmail.com>
2015-03-29 12:47:55 -0700

    Looping with ng-repeat

--- empty-projects/ng-todo/js/ngTodoCtrl.js
+++ empty-projects/ng-todo/js/ngTodoCtrl.js
@@ -1,9 +1,9 @@
 ngTodo.controller('NgTodoCtrl', function NgTodoCtrl($scope){
-  $scope.todo = [
+  $scope.todos = [
     { val: 'A new cool thing 1', completed: true },
     { val: 'A new cool thing 2', completed: false },
     { val: 'A new cool thing 3', completed: false },
     { val: 'A new cool thing 4', completed: true },
-    { val: 'A new cool thing 5', completed: false },
+    { val: 'A new cool thing 5', completed: false }
   ];
 });
