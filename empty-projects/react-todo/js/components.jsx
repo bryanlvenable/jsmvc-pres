b9241814bba5e597609bf6ab99e8e5fbab50faaa
Bryan L. Venable <bryanlvenable@gmail.com>
2015-03-30 15:19:42 -0700

    creating components

--- empty-projects/react-todo/js/components.jsx
+++ empty-projects/react-todo/js/components.jsx
@@ -7,12 +7,41 @@ app.components = app.components || {};
   'use strict';
 
   var TodoApp = app.components.TodoApp = React.createClass({
+    getInitialState: function() {
+      return {
+        todos: []
+      };
+    },
     render: function(){
       return (
         <div className="outer-container">
-          <p>interesting text goes here</p>
+          <NewTodo />
+          <TodoList />
+          <ClearCompleted />
         </div>
       );
     }
-  })
+  });
+
+  var NewTodo = app.components.NewTodo = React.createClass({
+    render: function() {
+      return (
+        <h1>New Todo</h1>
+      );
+    }
+  });
+  var TodoList = app.components.TodoList = React.createClass({
+    render: function() {
+      return (
+        <h1>Todo List</h1>
+      );
+    }
+  });
+  var ClearCompleted = app.components.ClearCompleted = React.createClass({
+    render: function() {
+      return (
+        <h1>Clear Completed</h1>
+      );
+    }
+  });
 })();
