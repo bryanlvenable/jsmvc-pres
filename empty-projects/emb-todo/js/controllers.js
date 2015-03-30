ca4f854d373360ff8588021ffd0555e2617f7348
Bryan L. Venable <bryanlvenable@gmail.com>
2015-03-30 00:51:23 -0700

    actions

--- empty-projects/emb-todo/js/controllers.js
+++ empty-projects/emb-todo/js/controllers.js
@@ -1,11 +1,23 @@
 Todos.TodoController = Ember.ObjectController.extend({
   actions: {
-    
+    removeTodo: function(){
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
+    }
   }
 });
 
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
-
+    createNewTodo: function() {
+      var newVal = this.get('newTodo');
+      var todo = this.store.createRecord('todo', {
+        val: newVal,
+        completed: false
+      });
+      this.set('newTodo', '');
+      todo.save();
+    }
   }
 });
